b9b57e055b5119fe8e5ae0e4eb5021b695b09472
Yudith Rubio <yudiht17@gmail.com>
2020-06-01 17:05:56 -0700

    Roles-Yudith

--- Maquillaje/Maquillaje.Web/Global.asax.cs
+++ Maquillaje/Maquillaje.Web/Global.asax.cs
@@ -1,3 +1,4 @@
+using Maquillaje.Web.Clase;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -15,7 +16,17 @@ namespace Maquillaje.Web
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            this.CheckRoles();
+            utilities.CheckSuperUser();
+            utilities.CheckClientDefault();
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private void CheckRoles()
+        {
+            utilities.CheckRoles("Admin");
+            utilities.CheckRoles("Client");
+
         }
     }
 }
