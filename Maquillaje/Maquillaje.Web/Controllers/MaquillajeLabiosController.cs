522a4b33a37f56d406fba0570184c0f1b6486ef1
Yudith Rubio <yudiht17@gmail.com>
2020-06-03 13:33:47 -0700

    MaquillajeLabiales-Vistas

---
+++ Maquillaje/Maquillaje.Web/Controllers/MaquillajeLabiosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Maquillaje.Web.Controllers
+{
+    public class MaquillajeLabiosController : Controller
+    {
+        // GET: MaquillajeLabios
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Maquillaje/Maquillaje.Web/Controllers/MaquillajeRostroController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Maquillaje.Web.Controllers
+{
+    public class MaquillajeRostroController : Controller
+    {
+        // GET: MaquillajeRostro
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
